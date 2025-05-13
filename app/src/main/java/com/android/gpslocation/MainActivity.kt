19e76451fb16440a658e5db7b71002ec9bb6c4ca
AdminConsultant <60258582+AdminConsultant@users.noreply.github.com>
2025-05-13 21:19:54 +0200

    Add files via upload

---
+++ app/src/main/java/com/android/gpslocation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.android.gpslocation
+
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Gestione dell'Edge-to-Edge (se desiderato)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
